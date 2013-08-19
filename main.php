a2dc8f9ee073970c14b199f460792a5fedb1c227
tikendramaitry <tikendramaitry@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-19 15:09:42 +0000

    updating to version 1.2.0
    git-svn-id: https://plugins.svn.wordpress.org/like-dislike-counter-for-posts-pages-and-comments/trunk@758508 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- main.php
+++ main.php
@@ -4,138 +4,88 @@ Plugin Name: Like Dislike counter
 Plugin URI: http://www.wpfruits.com
 Description: Like dislike counter for posts and comments
 Author: WPFruits
-Version: 1.1
+Version: 1.2.0
 Author URI: http://www.wpfruits.com
 */
-function like_dislike_counter_install() 
-	{
-		global $wpdb;
-		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-		$table_name = $wpdb->prefix."like_dislike_counters";     
-     	if($wpdb->get_var("show tables like '$table_name'") != $table_name) 
-     	{
-     	$sql= "CREATE TABLE ".$table_name."(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,post_id VARCHAR( 200 ) NOT NULL,ul_key VARCHAR( 30 ) NOT NULL,ul_value VARCHAR( 30 ) NOT NULL);"; 
-     	dbDelta($sql);
-	 	}
-      	
-   }
+
+require_once('ldclite-appearance.php');
+require_once('ldclite-function.php');
+
+// Runs when plugin is activated and creates new database field
 register_activation_hook(__FILE__,'like_dislike_counter_install');
+add_action('admin_init', 'ldclite_plugin_redirect');
+function ldclite_plugin_activate() {
+    add_option('ldclite_plugin_do_activation_redirect', true);
+}
+
+function ldclite_plugin_redirect() {
+    if (get_option('ldclite_plugin_do_activation_redirect', false)) {
+        delete_option('ldclite_plugin_do_activation_redirect');
+        wp_redirect('admin.php?page=ldc-main');
+    }
+}
+function like_dislike_counter_install() 
+{
+	add_option('ldclite_options', ldc_lite_defaultOptions());
+	global $wpdb;
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+	$table_name = $wpdb->prefix."like_dislike_counters";     
+	if($wpdb->get_var("show tables like '$table_name'") != $table_name){
+		$sql= "CREATE TABLE ".$table_name."(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,post_id VARCHAR( 200 ) NOT NULL,ul_key VARCHAR( 30 ) NOT NULL,ul_value VARCHAR( 30 ) NOT NULL);"; 
+		dbDelta($sql);
+	}	
+	ldclite_plugin_activate();
+}
+
+function ldc_lite_defaultOptions(){
+	$default = array(
+		'ldc_deactivate'  => 1,
+		'ldc_like_text' => 'Likes',
+		'ldc_dislike_text' => 'Dislikes'
+    );
+	return $default;
+}
+
+function ldclite_get_version(){
+	if ( ! function_exists( 'get_plugins' ) )
+	require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	$plugin_folder = get_plugins( '/' . plugin_basename( dirname( __FILE__ ) ) );
+	$plugin_file = basename( ( __FILE__ ) );
+	return $plugin_folder[$plugin_file]['Version'];
+}
 
 function like_dislike_couter_init_method() {
     wp_enqueue_script( 'jquery' );
+	if(is_admin()){
+		wp_enqueue_script('like-dislike',plugins_url( 'js/ldc-lite-admin.js' , __FILE__ ),array('jquery'),'1.0' );
+	}
+	if(is_admin()){
+		wp_register_style($handle = 'like-dislike-admin', plugins_url( 'css/ldc-lite-admin.css' , __FILE__ ), $deps = array(), $ver = '1.0.0', $media = 'all');
+		wp_enqueue_style('like-dislike-admin');
+	}
 }    
- 
 add_action('init', 'like_dislike_couter_init_method');
 
+function ldc_lite_enqueue_css(){
+	if(!is_admin()){
+		wp_register_style($handle = 'like-dislike', plugins_url( 'css/ldc-lite.css' , __FILE__ ), $deps = array(), $ver = '1.0.0', $media = 'all');
+		wp_enqueue_style('like-dislike');
+	}
+}
+add_action('wp_print_styles', 'ldc_lite_enqueue_css');
 
-function like_counter_p($text="Likes: ",$post_id=NULL)
-{
-	global $post;
-	if(empty($post_id))
-	{
-	$post_id=$post->ID;
-	}
-	echo "<span class='ul_cont' onclick=\"alter_ul_post_values(this,'$post_id','like')\" >".$text."<img src=\"".plugins_url( 'images/up.png' , __FILE__ )."\" />(<span>".get_post_ul_meta($post_id,"like")."</span>)</span>";
-}
-
-function dislike_counter_p($text="dislikes: ",$post_id=NULL)
-{
-	global $post;
-	if(empty($post_id))
-	{
-	$post_id=$post->ID;
-	}
-	echo "<span class='ul_cont' onclick=\"alter_ul_post_values(this,'$post_id','dislike')\" >".$text."<img src=\"".plugins_url( 'images/down.png' , __FILE__ )."\" />(<span>".get_post_ul_meta($post_id,"dislike")."</span>)</span>";
-}
-
-function like_counter_c($text="Likes: ",$post_id=NULL)
-{
-	global $comment;
-	if(empty($post_id))
-	{
-	$post_id=get_comment_ID();
-	}
-	echo "<span class='ul_cont' onclick=\"alter_ul_post_values(this,'$post_id','c_like')\" >".$text."<img src=\"".plugins_url( 'images/up.png' , __FILE__ )."\" />(<span>".get_post_ul_meta($post_id,"c_like")."</span>)</span>";
-}
-function dislike_counter_c($text="dislikes: ",$post_id=NULL)
-{
-	global $comment;
-	if(empty($post_id))
-	{
-	$post_id=get_comment_ID();
-	}
-	echo "<span class='ul_cont' onclick=\"alter_ul_post_values(this,'$post_id','c_dislike')\" >".$text."<img src=\"".plugins_url( 'images/down.png' , __FILE__ )."\" />(<span>".get_post_ul_meta($post_id,"c_dislike")."</span>)</span>";
-}
-function get_post_ul_meta($post_id,$up_type)
-{
-	global $wpdb;
-	$table_name = $wpdb->prefix."like_dislike_counters"; 
-	$sql="select ul_value from $table_name where post_id=$post_id and ul_key='$up_type' ;";
-	$to_ret=$wpdb->get_var($sql);
-	if(empty($to_ret))
-	{
-	$to_ret=0;
-	}
-	return $to_ret;
-}
-function update_post_ul_meta($post_id,$up_type)
-{
-	global $wpdb;
-	$table_name = $wpdb->prefix."like_dislike_counters";
-	$lnumber=get_post_ul_meta($post_id,$up_type);
-	if($up_type=='c_like'||$up_type=='c_dislike')
-	{
-	$for_com='c_';
-	}
-	else
-	{
-	$for_com='';
-	}
-	if($lnumber)
-	{ 
-	$sql="update $table_name set ul_value=".($lnumber+1)." where post_id='$post_id' and ul_key='$up_type';";
-		if(isset($_COOKIE['ul_post_cnt']))
-		{
-			$posts=$_COOKIE['ul_post_cnt'];
-			array_push($posts,$for_com.$post_id);
-			foreach($posts as $key=>$value)
-			{
-			setcookie("ul_post_cnt[$key]",$value, time()+1314000);
-			}
-		}
-		else
-		{
-		setcookie("ul_post_cnt[0]",$for_com.$post_id, time()+1314000);
-		}
-		$wpdb->query($sql);
-	}
-	else
-	{
-		$sql="insert into $table_name(post_id,ul_key,ul_value) values('$post_id','$up_type',".($lnumber+1).");";
-		if(isset($_COOKIE['ul_post_cnt']))
-		{
-			$posts=$_COOKIE['ul_post_cnt'];
-			array_push($posts,$post_id);
-			foreach($posts as $key=>$value)
-			{
-			setcookie("ul_post_cnt[$key]",$for_com.$value, time()+1314000);
-			}
-		}
-		else
-		{
-		setcookie("ul_post_cnt[0]",$for_com.$post_id, time()+1314000);
-		}
-	$wpdb->query($sql);
-	}
-}
 
 function wp_dislike_like_footer_script() {
-	if(!is_admin())
-	{
+	if(!is_admin()){
 	?>
     <script type="text/javascript">
-    function alter_ul_post_values(obj,post_id,ul_type)
-	{
+	var isProcessing = false; 
+    function alter_ul_post_values(obj,post_id,ul_type){
+	
+		if (isProcessing)    
+		return;  
+		isProcessing = true;   
+		
 		jQuery(obj).find("span").html("..");
     	jQuery.ajax({
    		type: "POST",
@@ -143,11 +93,11 @@ function wp_dislike_like_footer_script() {
    		data: "post_id="+post_id+"&up_type="+ul_type,
    		success: function(msg){
      		jQuery(obj).find("span").html(msg);
+			isProcessing = false; 
    			}
  		});
 	}
 	</script>
-    
     <?php
     }
 }
--- readme.txt
+++ readme.txt
@@ -1,70 +1,81 @@
 === Like Dislike Counter===
-Contributors: rahulbrilliant2004, tikendramaitry
-Donate link: http://www.wpfruits.com
-Tags: counter, like, dislike, comments, posts, pages
+Contributors: tikendramaitry, rahulbrilliant2004, Nishant Jain
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=wpfruits@gmail.com&item_name=Feedburner-Follow%20Me%20Popup%20And%20Widget&item_number=WordPress.org%20Donation&currency_code=USD&lc=US
+Tags: like counter, dislike counter,like, dislike, most liked posts and comments, recently liked posts and comments, thumbs down, thumbs up, like dislike page, like dislike post, like dislike comments
 Requires at least: 2.8
-Tested up to: 3.4
-Stable tag: 1.01
+Tested up to: 3.6
+Stable tag: 1.2.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-A simple like dislike counter for posts, pages and comments.   
+A simple ajax based WordPress Plugin which allows you to like or dislike posts, pages and comments and shows the like dislike counter as well.   
 
 == Description ==
-Like Dislike counter plugin is simple plugin which show likes and dislikes for a particular(post, page or a comment). Its very simple to use. You just need to write a simple code and the text that you want to diplay. It counts unique likes/dislikes. 
 
-Read more instructions about how to use here http://www.wpfruits.com/downloads/wp-plugins/wp-like-dislike-counter-plugin/
+Like Dislike counter plugin allows to integrate Like, Dislike Button into your WordPress website to allow your visitors to like and dislike pages, posts, custom post types and comments anonymously. Its very simple to use. You even have to write a single line of code to setup this plugin. It counts unique likes/dislikes for post/pages/comments. 
 
-=Features  =
-    This plugin adds simple like dislike counter to posts and pages or comments as you put the code in the page templates. 
+* Read more instructions about how to use here http://www.wpfruits.com/downloads/wp-plugins/wp-like-dislike-counter-plugin/
 
+== Features ==
+* Great Interface.
+* Ajax feature to update the data without reloading.
+* Visitors do not have to register or log in to use the Like Button.
+* Like-Dislike on Page, Post, Archive Pages and Comments.
+* Like Dislike count for Page, Post, Archive Pages and Comments.
+* Compatible with WP version 3.0 & above.
+* Easy Backend.
+* Clean Design.
+* Compatible with Multisite.
+* Added automatically (no Code required).
+* Template code ready.
+* Custom Like-Dislike Texts.
+* No Back Link needed.
+* Widget to show 'Most Liked' & generate more traffic.(Pro Version)
+* 10 pre-designed Templates.(Pro Version)
+* Freedom to Upload Custom Icon Set.(Pro Version)
+* Different Appearance Settings for Comments & Pages.(Pro Version)
+* 24 x 7 Premium Support.(Pro Version)
+* Shortcode to show like dislike buttons within content.(Pro Version)
+* Enable/Disable for Post Page, Archive Pages(Tags,Authors,Category,Date etc.) and Comments.(Pro Version)
+* Display different iconts for Post/Page and Comments.(Pro Version)
 
+**PRO Plugin URL:** <a href="http://www.sketchthemes.com/plugins/like-dislike-counter-pro-wordpress-plugin/
+" target="_blank">Like Dislike Pro Plugin Url</a>
+**PRO Plugin Demo URL:** <a href="http://plugins.wpfruits.com/like-dislike/
+" target="_blank">Like Dislike Pro DEmo</a>
 == Installation ==
-
-0. Upload `like-dislike-counter` to the `/wp-content/plugins/` directory
+1. Upload `like-dislike-counter-for-posts-pages-and-comments` to the `/wp-content/plugins/` directory.
 1. Activate the plugin through the 'Plugins' menu in WordPress
-2. This plugin will not automatically added to your theme. You need to add php lines manually.
-3. For Posts use the code in loop
-	For like(with in php tags)
-	 if(function_exists('like_counter_p')) { like_counter_p("text for like"); } 
+2. Activate the plugin via the 'Plugins' menu in WordPress, you'll automatically redirected to plugin admin panel.
+3. Plugin settings are located in 'Like Dislike Lite'.
 
-	For Dislike(with in php tags)
-	 if(function_exists('dislike_counter_p')) { dislike_counter_p("text for un-like"); } 
-
-	Parameter provide is optional. HTML can also be used as parameter.
-4. For Comments use the code in loop
-
-	For like(with in php tags)
-	 if(function_exists('like_counter_c')) { like_counter_c("text for like"); } 
-
-	For Dislike(with in php tags)
-	 if(function_exists('dislike_counter_c')) { dislike_counter_c("text for dislike"); } 
-
-5.Does not have any backend admin panel.
-
-	Thanks for using this plugin
+Thanks for using this plugin.
 
 For More Details please visit http://www.wpfruits.com/downloads/wp-plugins/wp-like-dislike-counter-plugin/
 
 == Frequently asked questions ==
 
-= A question that someone might have =
-
-An answer to that question.
+= Q: Can this be used for post, page and comments as well. =
+A: Yes it can be used for post, page and comments.
 
 == Screenshots ==
-
+1. How to use **Template Code.**
+2. Like Dislike Counter **General Settings.**
+3. Liked Dislike Counter **Front Appearance.**
 
 
 == Changelog ==
-1.1
-Updated plugin to show down.png in comments too. Earlier it was showing up.png for both like and dislike. 
 
+= Version 1.2.0 =
+* Added admin panel for plugin
+* Added code to auto integrate like-dislike button on post/page and comments section.
+* Fixed some issues with the WordPress 3.6
+* Updated front like dislike button appearance.
 
+= Version 1.1.0 =
+* Updated plugin to show down.png in comments too. Earlier it was showing up.png for both like and dislike. 
 
 == Upgrade notice ==
+* Upgrade quickly for better performance of plugin.
 
-
-
-== Arbitrary section 1 ==
-
+== Arbitrary section ==
